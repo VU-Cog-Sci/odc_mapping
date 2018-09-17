46190d3c70c6e3c18261ab75f0c0e10047ff12fd
Gilles86 <Gilles.de.Hollander@gmail.com>
2018-09-17 19:42:24 +0200

    Include manual edits in CRUISE

--- analysis/structural/segment.py
+++ analysis/structural/segment.py
@@ -4,34 +4,57 @@ from bids.grabbids import BIDSLayout
 import argparse
 import os
 from nighres import brain
-
+import warnings
+from nilearn import image
+from scipy import ndimage
+import nibabel as nb
 
 def main(sourcedata, 
          derivatives,
          tmp_dir,
          subject=None):
 
-    skullstrip_layout = BIDSLayout(os.path.join(derivatives,
-                                                'nighres',
-                                                'skullstrip'))
-
     dura_layout = BIDSLayout(os.path.join(derivatives,
-                                                'masks'))
-
+                                          'masks'))
     dura_mask = get_bids_file(dura_layout, subject, 'dura')
-    print(dura_mask)
+    
+    layout = BIDSLayout('/sourcedata')
 
-    t1w_masked = get_bids_file(skullstrip_layout, subject, 't1w', 'UNI')
-    t1map_masked = get_bids_file(skullstrip_layout, subject, 't1map', 'T1map')
+    t1w = get_bids_file(layout, subject, 'UNI')
+    t1map = get_bids_file(layout, subject, 'T1map')
+
+    fmriprep_layout = BIDSLayout('/derivatives/fmriprep')
+    brain_mask = get_bids_file(fmriprep_layout, subject, 'brainmask')
+
+    brain_mask_dil = ndimage.binary_dilation(nb.load(brain_mask).get_data(), iterations=3)
+    brain_mask = image.new_img_like(brain_mask, brain_mask_dil)
+
+    brain_mask = image.resample_to_img(brain_mask, t1w)
+    brain_mask.to_filename('/derivatives/mask_test.nii.gz')
+    
+    t1w_masked = image.math_img('brain_mask * t1w', 
+                                 brain_mask=brain_mask,
+                                 t1w=t1w)
+
+
+    t1map_masked = image.math_img('brain_mask * t1map', 
+                                  brain_mask=brain_mask,
+                                  t1map=t1map)
+
+    t1w_masked_fn = '/derivatives/fmriprep/sub-{subject}/anat/sub-{subject}_T1w_masked.nii.gz'.format(subject=subject)
+    t1w_masked.to_filename(t1w_masked_fn)
+
+    t1map_masked_fn = '/derivatives/fmriprep/sub-{subject}/anat/sub-{subject}_T1map_masked.nii.gz'.format(subject=subject)
+    t1map_masked.to_filename(t1map_masked_fn)
     
     output_dir = os.path.join(derivatives,
                               'nighres',
-                              'segmentation',
+                              'segmentation_dilation',
                               'sub-{}'.format(subject))
 
-    brain.mgdm_segmentation(contrast_image1=t1w_masked,
+    brain.mgdm_segmentation(contrast_image1=t1w_masked_fn,
                             contrast_type1='Mp2rage7T',
-                            contrast_image2=t1map_masked,
+                            contrast_image2=t1map_masked_fn,
                             contrast_type2='T1map7T',
                             contrast_image3=dura_mask,
                             contrast_type3='Filters',
@@ -53,8 +76,9 @@ def get_bids_file(layout,
         img = [im for im in img if filter in im]
 
     if len(img) > 1:
-        warnings.warn('Found more than one {}-image, using {}'.format(modality,
-                                                                      img[0]))
+        warnings.warn('Found more than one {}-image:\n'
+                      '{}'
+                       'using {}'.format(modality, img, img[0]))
 
     return img[0]
 
