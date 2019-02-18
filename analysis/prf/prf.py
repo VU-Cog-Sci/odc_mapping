5522bd2c24c7fc291165053db095987cd08c5cf1
Gilles de Hollander <gilles.de.hollander@gmail.com>
2019-02-18 17:55:44 +0100

    First version that can use correlation method to find best parameters

--- analysis/prf/prf.py
+++ analysis/prf/prf.py
@@ -6,6 +6,10 @@ from popeye.visual_stimulus import VisualStimulus
 import popeye.og as OGModel
 import popeye.utilities as utils
 from scipy import signal
+from scipy import interpolate
+from scipy import optimize
+from popeye.utilities import gradient_descent_search, error_function, coeff_of_determination
+import pandas as pd
 
 class PRFRidgeModel(object):
     
@@ -127,33 +131,131 @@ class PRFGridSearch(object):
         for i, (x, y, s) in enumerate(zip(self.xs.ravel(), self.ys.ravel(), self.sizes.ravel())):
             self.predictions[:, i] = self.model_func.generate_prediction(x, y, s, 1, 0)        
         
-    def fit(self, data):
+    def fit(self, data,
+            include_intercept_slope=False,
+            include_predictions=False):
         if self.predictions is None:
             raise Exception('First use self.make_predictions!')
         
-        self.predictions_ = signal.resample(self.predictions, data.shape[0], axis=0)
-        self.predictions_ -= self.predictions_.mean(0)[np.newaxis, :]
-        
+        self.predictions_ = self.predictions - self.predictions.mean(0)[np.newaxis, :]
+        self.data = data
         self.data_ = data - data.mean(0)[np.newaxis, :]
         
         # Calculate R^2
+        print('Calculate R2')
         self.num = (np.dot(self.predictions_.T, self.data_) / self.data_.shape[0])
         self.denom = self.predictions_.std(0)[:, np.newaxis] * self.data_.std(0)[np.newaxis, :]
         self.r2 = (self.num / self.denom)**2
         self.r2[np.isnan(self.r2)] = 0
         
-        print(self.r2.shape)
-        best_pars = np.argmax(self.r2, 0)
-        print(best_pars.shape, self.angles.shape)
+        print('Find best R2')
+        self.best_pars_ix = np.argmax(self.r2, 0)
         
-        self.best_x, self.best_y, self.best_s = self.xs.ravel()[best_pars], self.ys.ravel()[best_pars], self.sizes.ravel()[best_pars]
-        self.best_angle = self.angles.ravel()[best_pars]
-        self.best_ecc = self.eccentricities.ravel()[best_pars]
+        print('Find best parameters')
+        self.best_x, self.best_y, self.best_s = self.xs.ravel()[self.best_pars_ix], self.ys.ravel()[self.best_pars_ix], self.sizes.ravel()[self.best_pars_ix]
+        self.best_angle = self.angles.ravel()[self.best_pars_ix]
+        self.best_ecc = self.eccentricities.ravel()[self.best_pars_ix]
 
-        return {'x':self.best_x, 
-                'y':self.best_y, 
-                'size':self.best_s,
-                'r2':self.r2.max(0),
-                'angle':self.best_angle,
-                'ecc':self.best_ecc}
-        
+        print('Store results')
+        results =  {'x':self.best_x,
+                    'y':self.best_y,
+                    'size':self.best_s,
+                    'r2':self.r2.max(0),
+                    'angle':self.best_angle,
+                    'ecc':self.best_ecc}
+
+        if include_intercept_slope or include_predictions:
+            print('Find intercept and slope')
+            results['intercept'] = self.data.mean(0)
+            results['slope'] = self.data.std(0) / self.predictions[:, self.best_pars_ix].std(0)
+
+
+        if include_predictions:
+            print('Make best fitting predictions')
+            self.predictions = results['intercept'] + results['slope'] * self.predictions_[:, self.best_pars_ix]
+
+        return pd.DataFrame(results)
+
+    def refine_parameters(self,
+                          results,
+                          correlation_method=True,
+                          verbose=1):
+
+        results = results.copy()
+        if correlation_method:
+            def make_zscored_prediction(x, y, size):
+           
+                pred = self.model_func.generate_prediction(x, y, size, 1, 0)
+                pred -= pred.mean()
+                pred /= pred.std()
+           
+                return pred
+
+        for ix, row in results.iterrows():
+            
+            d = self.data[:, ix]
+
+            if correlation_method:
+                print('Correlation method')
+                ballpark = row.x, row.y, row.size
+                bounds = ((self.xs.min(), self.xs.max()),
+                                  (self.ys.min(), self.ys.max()),
+                                  (self.sizes.min(), self.sizes.max()))
+
+                d_ =  d - d.mean()
+                d_ /= d_.std()
+
+                r = gradient_descent_search(d_,
+                                        error_function,
+                                        make_zscored_prediction,
+                                        ballpark,
+                                        bounds,
+                                        0)
+
+                results.loc[ix, 'x_opt'] = r[0][0]
+                results.loc[ix, 'y_opt'] = r[0][1]
+                results.loc[ix, 's_opt'] = r[0][2]
+
+
+                X = np.ones((len(d_), 2))
+                X[:, 1] = make_zscored_prediction(*r[0])
+
+                beta, residuals, _, _ = np.linalg.lstsq(X, d)
+
+                results.loc[ix, 'baseline_opt'] = beta[0]
+                results.loc[ix, 'amplitude_opt'] = beta[1]
+
+                results.loc[ix, 'r2_opt'] = 1 - residuals / (len(d) * d.var())
+                results.loc[ix, 'estimation method'] = 'correlation method'
+
+            else:
+                print('Classic method')
+                ballpark = row.x, row.y, row.size, row.slope, row.intercept
+                bounds = ((self.xs.min(), self.xs.max()),
+                                  (self.ys.min(), self.ys.max()),
+                                  (self.sizes.min(), self.sizes.max()),
+                                  (0, self.data[:, ix].std() * 3),
+                                  (self.data_[:, ix].min(), self.data_[:, ix].max()))
+
+                r = gradient_descent_search(d,
+                                        error_function,
+                                        self.model_func.generate_prediction,
+                                        ballpark,
+                                        bounds,
+                                        0)
+                results.loc[ix, 'x_opt'] = r[0][0]
+                results.loc[ix, 'y_opt'] = r[0][1]
+                results.loc[ix, 's_opt'] = r[0][2]
+                results.loc[ix, 'baseline_opt'] = r[0][3]
+                results.loc[ix, 'amplitude_opt'] = r[0][4]
+
+                pred = self.model_func.generate_prediction(*r[0])
+
+                results.loc[ix, 'r2_opt'] = coeff_of_determination(d, pred) / 100
+
+                results.loc[ix, 'estimation method'] = 'Classic method'
+
+            print("Used {} function evaluations".format(r[4]))
+
+        return results
+
