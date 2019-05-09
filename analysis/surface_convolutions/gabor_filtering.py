5d9da991a8fa2e43f75700cf849cf02913f1ddde
Gilles de Hollander <gilles.de.hollander@gmail.com>
2019-05-09 16:55:29 +0200

    Some cleaning up

--- analysis/surface_convolutions/gabor_filtering.py
+++ analysis/surface_convolutions/gabor_filtering.py
@@ -1,134 +1,108 @@
-import cortex
-import yaml
-import os
-import os.path as op
-import pandas as pd
-from scipy import stats
-import numpy as np
+import argparse
 from nilearn import surface
-import pandas as pd
 import pickle as pkl
-
-subject = 'tr'
-session = 'odc'
-derivatives = '/data/odc/derivatives'
-derivatives = '/derivatives'
-
-zmap = op.join(derivatives, 'sampled_giis', 'sub-{subject}', 'ses-{session}',
-               'func', 'sub-{subject}_ses-{session}_left_over_right_desc-zmap-depth-0.571_hemi-lh.gii'). \
-    format(subject=subject, session=session)
-
-zmap = surface.load_surf_data(zmap)
-
-left, right = cortex.db.get_surf('odc.{}'.format(subject), 'fiducial')
-left_surface = cortex.polyutils.Surface(left[0], left[1])
-right_surface = cortex.polyutils.Surface(right[0] , right[1])
-
-patch_fn = op.join(derivatives, 'coordinate_patches',
-                   'sub-{subject}',
-                   'anat',
-                   'sub-{subject}_hemi-lh_coordinatepatch.pkl').format(**locals())
-
-with open(patch_fn, 'rb') as f:
-    patch = pkl.load(f)
+import numpy as np
+import os.path as op
+import os
+import pandas as pd
+from scipy import interpolate
+from itertools import product
+from tqdm import tqdm
+from skimage.filters import gabor_kernel
+import scipy.ndimage as ndi
 
 
-fwhm = 10
-orientations = np.linspace(0, np.pi, 8, endpoint=False)
-wavelengths = np.arange(1, 11, 1)
+def main(derivatives,
+         subject,
+         session):
 
-def make_gaussian_patches(distances,
-                          fwhm):
+    grid_resolution = .35
+    max_frequency = 1.
+    min_frequency = 1/10.
+    n_frequencies = 20
+    scale_factor = (min_frequency/max_frequency)**(-1/(n_frequencies-1))
+    frequencies_mm = scale_factor**-np.arange(0, n_frequencies) * max_frequency
+    orientations = np.linspace(0, np.pi, 4, endpoint=False)
 
-    sd = fwhm / 2.355
-    gauss = stats.norm(0, sd).pdf(distances)
+    frequencies_pix = frequencies_mm * grid_resolution
 
-    return gauss
+    for hemi in ['lh', 'rh']:
 
-def make_gabor_patches(coords,
-                       wavelength,
-                       theta=0):
+        with open(op.join(derivatives, 'coordinate_patches',
+                          'sub-{subject}',
+                          'anat',
+                          'sub-{subject}_hemi-{hemi}_coordinatepatch.pkl').format(**locals()),
+                          'rb') as f:
+            patch = pkl.load(f)
 
-    coords_ = coords[..., 0] * np.cos(theta) + coords[..., 1] * np.sin(theta)
+        xy = pd.DataFrame({'x':patch['coordinates'][0, :],
+                           'y':patch['coordinates'][1, :]})
 
-    gabor = np.exp(1j * (2 * np.pi * coords_ / wavelength))
-    gabor.real -= gabor.real.mean(axis=1)
-    gabor.imag -= gabor.imag.mean(axis=1)
+        depths = np.round(np.linspace(0, 1, 8), 3)[1:-1]
 
-    gabor.real /= np.linalg.norm(gabor.real, axis=1)
-    gabor.imag /= np.linalg.norm(gabor.imag, axis=1)
+        pb = tqdm(total=len(frequencies_pix) * len(orientations) * len(depths))
 
-    return gabor
+        results = []
 
-results_dir = op.join(derivatives, 'zmap_spatfreq', 'sub-{subject}', 'ses-{session}',
-                      'func').format(subject=subject,
-                                     session=session)
+        for depth in depths:
+            zmap = op.join(derivatives, 'sampled_giis', 'sub-{subject}', 'ses-{session}',
+                           'func',
+                           'sub-{subject}_ses-{session}_left_over_right_desc-zmap-depth-{depth}_hemi-{hemi}.gii'). \
+                format(**locals())
+            zmap_v = surface.load_surf_data(zmap)
+            zmap = pd.DataFrame(zmap_v, columns=['z_value'])
+            zmap = zmap[patch['vertex_mask']]
+            df = zmap.merge(xy, left_index=True, right_index=True)
 
-if not op.exists(results_dir):
-    os.makedirs(results_dir)
+            x_grid, y_grid = np.meshgrid(np.arange(df['x'].min(), df['x'].max(), grid_resolution),
+                                         np.arange(df['y'].min(), df['y'].max(), grid_resolution))
 
-ss = left_surface.create_subsurface(patch['vertex_mask'])
-
-distances = patch['distances']
-coords = patch['vertexwise_coordinate_system']
-
-
-xy = pd.DataFrame({'x':patch['coordinates'][0, :],
-                   'y':patch['coordinates'][1, :]})
-
-xy_ = xy.loc[ss.subsurface_vertex_mask]
-example_index = np.linalg.norm(xy_ - xy_.mean(), axis=1).argmin()
-
-energies = []
-gauss = make_gaussian_patches(distances, fwhm)
-
-# Do gaussian convolution and remove dc-norm
-zmap_gauss_norm = (gauss * zmap[np.newaxis, ss.subsurface_vertex_mask]) - (gauss * zmap[np.newaxis, ss.subsurface_vertex_mask]).mean(1)[:, np.newaxis]
-
-example_gabors = []
-
-for wl in wavelengths:
-    for ori in orientations:
-        print(wl, ori)
-        gabor = make_gabor_patches(coords,
-                                   wl,
-                                   ori)
-
-        energy = gabor * zmap_gauss_norm
-        energy = np.linalg.norm(energy.real, axis=1) + np.linalg.norm(energy.imag, axis=1)
-
-        example_gabors.append(np.real(gauss[example_index] * gabor[example_index]))
-
-        index = pd.MultiIndex.from_tuples([(wl, ori)], names=['wavelength', 'orientation'])
-        energy = pd.DataFrame(energy[np.newaxis, :], columns=xy_.index, index=index)
-        energies.append(energy)
-
-energies = pd.concat(energies)
-
-#max_wavelength = energies.groupby('wavelength').sum().idxmax(0)
-#max_orientation = energies.T.apply(lambda row: row[max_wavelength.loc[row.name]].idxmax(), 1)
-
-energies.to_pickle(op.join(results_dir, 'sub-{subject}_ses-{session}_fwhm-{fwhm}_energies.pkl').format(subject=subject,
-                                                                                                   fwhm=fwhm,
-                                                                                           session=session))
-
-example_gabors = pd.DataFrame(example_gabors,
-                              index=energies.index,
-                              columns=xy_.index)
-
-example_gabors.to_pickle(op.join(results_dir, 'sub-{subject}_ses-{session}_fwhm-{fwhm}_example_gabors.pkl').format(subject=subject,
-                                                                                                 session=session,
-                                                                                                               fwhm=fwhm))
-
-#v_dominant_orientations = cortex.Vertex(ss.lift_subsurface_data(dominant_orientations),
-                                    #'odc.{}'.format(subject))
-#v_dominant_wavelengths = cortex.Vertex(ss.lift_subsurface_data(dominant_wavelengths),
-                                    #'odc.{}'.format(subject))
-#v_example_gabors = cortex.Vertex(ss.lift_subsurface_data(np.array(example_gabors)),
-                                    #'odc.{}'.format(subject))
-#ds = cortex.Dataset(ori=v_dominant_orientations,
-                #wl=v_dominant_wavelengths,
-                #gabors=v_example_gabors)
+            data = interpolate.griddata(df[['x', 'y']],
+                                        df['z_value'],
+                                        np.vstack((x_grid.ravel(), y_grid.ravel())).T).reshape(x_grid.shape)
 
 
 
+            for freq, ori in product(frequencies_pix, orientations):
+                kernel = gabor_kernel(freq, ori, n_stds=3)
+                filtered_real = ndi.convolve(data, np.real(kernel), mode='wrap')
+                filtered_imag = ndi.convolve(data, np.imag(kernel), mode='wrap')
+                power = np.sqrt(filtered_real**2 + filtered_imag**2)
+
+                power = pd.DataFrame([power.ravel()],
+                                     index=pd.MultiIndex.from_tuples([(depth, freq/grid_resolution, ori)], names=['depth', 'frequency', 'orientation']))
+                results.append(power)
+                pb.update()
+
+        results = pd.concat(results, axis=0)
+        results_vertex = interpolate.griddata(np.vstack((x_grid.ravel(), y_grid.ravel())).T,results.T, xy.loc[zmap.index])
+
+        results_vertex = pd.DataFrame(results_vertex.T,
+                                      index=results.index,
+                                      columns=zmap.index)
+
+        results_dir = op.join(derivatives, 'zmap_spatfreq',
+                                         'sub-{subject}',
+                                         'ses-{session}',
+                                         'func').format(**locals())
+        if not op.exists(results_dir):
+            os.makedirs(results_dir)
+
+        results_vertex.to_pickle(op.join(results_dir,
+                                         'sub-{subject}_ses-{session}_hemi-{hemi}_energies.pkl').format(**locals()))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("subject", 
+                        type=str,
+                        help="subject to process")
+    parser.add_argument("session", 
+                        type=str,
+                        help="subject to process")
+
+    args = parser.parse_args()
+
+    main('/data/odc/derivatives',
+         subject=args.subject,
+         session=args.session)
